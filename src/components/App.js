633a4b199bacb8c676d3d44297acfc3c17d66098
kickvire <varuit2020@gmail.com>
2020-11-30 12:14:02 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,47 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import { useLocation, BrowserRouter, Link } from "react-rou ter-dom";
+import { Route, Switch } from "react-router-dom";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="main">
+        <BrowserRouter>
+          <LocationDisplay />
+          <Switch>
+            <Route exact path="/about">
+              <>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <div>You are on the about Page</div>
+              </>
+            </Route>
+            <Route exact path="/">
+              <>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Home />
+              </>
+            </Route>
+            <Route path="*">
+              <div>No match</div>
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
+function Home() {
+  return <div>You are home</div>;
+}
 
+export function LocationDisplay() {
+  const { pathname } = useLocation();
+  return <div data-testid="location-display">{pathname}</div>;
+}
+
+// export { LocationDisplay };
 export default App;
